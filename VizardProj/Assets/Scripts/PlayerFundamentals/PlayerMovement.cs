0d343d4a94fc80b7054dc460c16cfbc0b5cc2761
Daniel Sohler <daniel.sohler73@gmail.com>
2022-05-03 12:04:33 +1000

    Added playermovement
    3 public values:
    moveSpeed = base speed player moves at
    speedMuliplier = multiplies base speed, for specific valye
    groundDrag = limits top speed

--- VizardProj/Assets/Scripts/PlayerFundamentals/PlayerMovement.cs
+++ VizardProj/Assets/Scripts/PlayerFundamentals/PlayerMovement.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Movement")]
+    public float moveSpeed;
+    [SerializeField] private float speedMultiplier;
+    public Transform orientation;
+    float horizonalInput;
+    float verticalInput;
+
+    public float groundDrag;
+
+    Vector3 moveDirection;
+    Rigidbody rb;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        MyInput();
+        SpeedControl();
+
+        rb.drag = groundDrag;
     }
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void MyInput()
+    {
+        horizonalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+
+    }
+
+    private void MovePlayer()
+    {
+        // movement direction math
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizonalInput;
+        rb.AddForce(moveDirection.normalized * moveSpeed * speedMultiplier, ForceMode.Force);
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        // limit velocity
+        if (flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+
 }
