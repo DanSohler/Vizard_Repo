fb11116ad4be2d2389bb41102e214e4ac4a065ec
Daniel Sohler <daniel.sohler73@gmail.com>
2022-05-10 16:44:22 +1000

    End of day Edits

--- VizardProj/Assets/Scripts/Verb Scripts/SpellManager.cs
+++ VizardProj/Assets/Scripts/Verb Scripts/SpellManager.cs
@@ -6,11 +6,6 @@ public class SpellManager : MonoBehaviour
 {
     public SlotManager slotManScript;
 
-    private void Awake()
-    {
-        slotManScript = GetComponent<SlotManager>();
-    }
-
     //Script should contain the verb recepies needed for executing verb spells
 
 }
